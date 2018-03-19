d02493cbacc3d061f9df23325859d15c65656dde
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-03-19 11:29:01 +0900

    Added SignalR nuget package.

---
+++ PerfSurf/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PerfSurf.Startup))]
+
+namespace PerfSurf
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Microsoft ASP.NET SignalR
+            // https://www.nuget.org/packages/Microsoft.AspNet.SignalR/
+            // Install-Package -Id Microsoft.AspNet.SignalR -ProjectName PerfSurf
+            app.MapSignalR();
+
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
