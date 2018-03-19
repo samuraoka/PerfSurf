29afa1642014ca9323ab2b07ad6efed3716d4efd
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-03-19 18:50:29 +0900

    Implemented some code for PerfClient clip.

--- PerfSurf/Scripts/perfSurf.js
+++ PerfSurf/Scripts/perfSurf.js
@@ -7,6 +7,34 @@
         model.addMessage(message);
     };
 
+    perfHub.client.newCounters = function (counters) {
+        model.addCounters(counters);
+    };
+
+    var ChartEntry = function (name) {
+        var self = this;
+
+        self.name = name;
+        self.chart = new SmoothieChart({
+            millisPerPixel: 50,
+            labels: { fontSize: 15 }
+        });
+        self.timeSeries = new TimeSeries();
+        self.chart.addTimeSeries(self.timeSeries, { lineWidth: 3, strokeStyle: "#00ff00" });
+    }
+
+    ChartEntry.prototype = {
+        addValue: function (value) {
+            self.timeSeries.append(new Date().getTime(), value);
+        },
+
+        start: function () {
+            var self = this;
+            self.canvas = document.getElementById(self.name);
+            self.chart.streamTo(self.canvas);
+        }
+    };
+
     // knockoutjs
     // https://www.nuget.org/packages/knockoutjs/
     // Install-Package -Id knockoutjs -ProjectName PerfSurf
@@ -14,9 +42,26 @@
         var self = this;
         self.message = ko.observable("");
         self.messages = ko.observableArray();
+        self.counters = ko.observableArray();
     };
 
     Model.prototype = {
+        addCounters: function (updatedCounters) {
+            var self = this;
+
+            $.each(updatedCounters, function (index, updateCounter) {
+                var entry = ko.utils.arrayFirst(self.counters(), function (counter) {
+                    return counter.name == updateCounter.name;
+                });
+                if (!entry) {
+                    entry = new ChartEntry(updateCounter.name);
+                    self.counters.push(entry);
+                    entry.start();
+                }
+                entry.addValue(updateCounter.value);
+            });
+        },
+
         sendMessage: function () {
             var self = this;
             perfHub.server.send(self.message());
