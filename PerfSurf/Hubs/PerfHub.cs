c34bb415af939748deacead44af8e63aa8e4ecdb
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-03-19 11:41:49 +0900

    Implemented some code for "Setting Up" clip.

---
+++ PerfSurf/Hubs/PerfHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace PerfSurf.Hubs
+{
+    public class PerfHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello("Hi!");
+        }
+    }
+}
